08a75e466082b0f45abdbc74381ac23f7fa99ad3
Bruno Miranda <brmiranda10@gmail.com>
2024-04-25 19:01:16 -0300

    cats names calling by a function

---
+++ chapter_4/allMyCats.py
@@ -0,0 +1,21 @@
+catNames = []
+
+
+def showCatNames():
+    global catNames
+    while True:
+        print(
+            "Enter the name of cat "
+            + str(len(catNames) + 1)
+            + "(Or enter nothing to stop.):"
+        )
+        name = input()
+        if name == "":
+            break
+        catNames.append(name)
+        ##catNames = catNames + [name]
+    return catNames
+
+
+for name in showCatNames():
+    print("The cat names are: " + catNames)
