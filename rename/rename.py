c2253ca8f02f68faa381c75ad4ab7392a089a12c
Bruno Miranda <brmiranda10@gmail.com>
2025-05-14 23:36:20 -0300

    adjusting code for renaming files

--- rename/rename.py
+++ rename/rename.py
@@ -1,16 +1,23 @@
 import os
 
+# Define o caminho para a pasta
+pasta = "C:\\Users\\miran\\OneDrive\\Python\\rename\\imagens_teste"
 
-def renomear_arquivos(pasta):
-    arquivos_png = sorted([f for f in os.listdir(pasta) if f.lower().endswith(".png")])
-    for indice, nome_antigo in enumerate(arquivos_png):
-        nome_novo = f"arquivo_{indice + 1:02d}.jpg"
+try:
+    # Lista todos os arquivos na pasta
+    arquivos = sorted([f for f in os.listdir(pasta) if f.lower().endswith(".jpg")])
+
+    # Renomeia os arquivos
+    for i, nome_antigo in enumerate(arquivos):
+        nome_novo = f"arquivo_{i+1:02d}.jpg"
         caminho_antigo = os.path.join(pasta, nome_antigo)
         caminho_novo = os.path.join(pasta, nome_novo)
         os.rename(caminho_antigo, caminho_novo)
-    print(f"Arquivos na pasta '{pasta}' renomeados com sucesso!")
+        print(f'Arquivo "{nome_antigo}" renomeado para "{nome_novo}"')
 
+    print("Arquivos renomeados com sucesso!")
 
-if __name__ == "__main__":
-    pasta_imagens = "."  # Use "." para a pasta atual, ou coloque o caminho completo da sua pasta aqui
-    renomear_arquivos(pasta_imagens)
+except FileNotFoundError:
+    print(f'Erro: A pasta "{pasta}" não foi encontrada.')
+except Exception as e:
+    print(f"Ocorreu um erro: {e}")
